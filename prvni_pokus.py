b6cbe1c777f7281bdacc26125461513fb2d5a4e2
petrleba <leba@souepl.cz>
2023-04-23 20:51:38 +0200

    kontrola vstupu

--- prvni_pokus.py
+++ prvni_pokus.py
@@ -1,4 +1,9 @@
-cislo = int(input("Zadejte číslo: "))
+def check_entry():
+    result = int(input("Zadejte číslo: "))
+    return result
+
+
+cislo = check_entry()
 
 if cislo % 2 == 0:
     print("Zadané číslo je sudé.")
